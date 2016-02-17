dfb4fefe64e02683c798868f2573e41816d5790d
= <josev@josev.com.br>
2016-02-17 20:41:00 -0200

    import locale

--- models/config.py
+++ models/config.py
@@ -1,3 +1,4 @@
 # from gluon.storage import Storage
 # vendaAtual = Storage()  #vendaAtual é uma session
 # vendaAtual.codigo = 5555  #vendaAtual{'codigo':555}
+
--- models/db.py
+++ models/db.py
@@ -14,7 +14,6 @@ from gluon.contrib.appconfig import AppConfig
 ## once in production, remove reload=True to gain full speed
 myconf = AppConfig(reload=True)
 
-
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
     db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
