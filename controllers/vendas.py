10c090610116638c22117acac77a07621ab3c4fc
python <josev@josev.com.br>
2016-05-25 13:58:05 -0300

    novo-layout: venda 70%

--- controllers/vendas.py
+++ controllers/vendas.py
@@ -4,6 +4,8 @@ def historico():
     mesAno = hoje.strftime('%m/%Y')
     hoje = hoje.strftime('%Y-%m')#pega apenas o ano e mes atual
 
+
+
     # se receber datas de inicio e fim
     if request.vars.inicial:
         formListar = db((db.historicoVendas.dataVenda >= request.vars.inicial) & (db.historicoVendas.dataVenda <= request.vars.final)).select()      
@@ -41,3 +43,229 @@ def imprimir():
         itens_parcelas = ""
         pass    
     return dict(historico_venda=historico_venda,itens_venda=itens_venda, itens_parcelas=itens_parcelas) 
+
+
+
+@auth.requires_login()
+def etapa_1():
+    if not session.parcelada:
+        session.parcelada = " "
+
+    representante = ''   
+    if session.representante:
+        representante = db(db.representantes.id == session.representante ).select('nome')[0].nome
+
+    # form cliente e representante
+    form = SQLFORM.factory(
+        Field('Cliente',default=session.cliente, requires = IS_NOT_EMPTY(error_message = "Digite o nome do cliente"
+            ), widget = SQLFORM.widgets.autocomplete(
+            request, db.clientes.nome, limitby=(0,5), min_length=1)),
+        #     # Field('Cliente',default=session.cliente , requires = IS_IN_DB(db, Clientes.nome, error_message = 'Escolha um cliente'),
+            # Field('Cliente', default=session.cliente, requires = IS_IN_DB(db, Clientes.nome, error_message = 'Escolha um representante') ),
+            Field('Representante',default=representante , requires = IS_IN_DB(db, Representantes.nome, error_message = 'Escolha um representante'))
+        )
+    if form.process().accepted:
+        session.cliente = form.vars.Cliente
+        session.idCliente = db(db.clientes.nome == form.vars.Cliente).select('id')[0].id
+        session.representante = db(db.representantes.nome == form.vars.Representante ).select('id')[0].id
+        #cria codigo da venda  
+        if not session.codigo_venda:
+            from datetime import datetime
+            now = datetime.now()
+            session.codigo_venda =  now.strftime("%y%m%d""%S%M%H")
+            pass    
+        redirect(URL('etapa_2')) 
+    
+
+    return dict(form=form)   
+
+
+# ------------------ ETAPA 2 ---------------------
+@auth.requires_login()
+def etapa_2():
+    grid = db(Itens.codigoVenda == session.codigo_venda).select() 
+    # ---- sTotal
+    sTotal = 0.0
+    for iten in grid:
+        sTotal += float(iten.valorTotal)  
+    session.sTotal = sTotal
+    # ---- fim sTotal
+    return dict(grid=grid, sTotal=sTotal)
+
+def produto():
+    codigo = request.vars.codigo
+    produto = request.vars.produto
+    qtde = request.vars.qtde
+
+
+    valorUn = (db(db.produtos.codigo_produto == codigo).select('preco_produto_lojinha'))[0].preco_produto_lojinha
+    valorTotal = int(qtde)*round(float(valorUn),2)
+    Itens.insert(codigoVenda=session.codigo_venda,codigoIten=codigo,produto=produto,quantidade=qtde,valorUnidade=valorUn,valorTotal=valorTotal)
+    #redirect(URL('etapa_2?menu=caixa')) 
+ 
+def delItem():
+    index = request.vars.transitory
+    # index = index.split(";")
+    db(Itens.id == index).delete()
+    # session.venda_sTotal = float(session.venda.sTotal) - float(index[1]) #subtração do valor dos itens
+
+def buscaCodigo():
+    cod = request.vars.transitory
+    iten = db(db.produtos.codigo_produto == "%s"%cod).select('nome_produto') 
+    # se o iten existir!
+    if iten:
+        ret =  "$('#no_table_qtde').attr('disabled',false).focus();$('#no_table_produto').val(%s);$('#no_table_codigo').css('color','#555').parent().removeClass('has-error')"% repr(iten[0].nome_produto)
+    else:
+        ret =  "$('#no_table_qtde').attr('disabled',true);$('#no_table_codigo').parent().addClass('has-error').children().focus().css('color','red');$('#no_table_produto').val('')"
+        pass
+    return ret
+
+def buscaProduto():
+    prod = request.vars.transitory
+    iten = db(db.produtos.nome_produto == "%s"%prod).select('codigo_produto') 
+    # se o iten existir!
+    if iten:
+        ret =  "$('#no_table_qtde').attr('disabled',false).focus();$('#no_table_codigo').val(%s);$('#no_table_produto').css('color','#555').parent().removeClass('has-error');$('input[type=submit]');"% repr(iten[0].codigo_produto)
+    else:
+        ret =  "$('#no_table_qtde').attr('disabled',true);$('#no_table_produto').parent().addClass('has-error').children().focus().css('color','red');$('#no_table_codigo').val('')"
+        pass
+    return ret
+# ------------------ FIM DA ETAPA 2 ---------------------
+
+# ----------------------- ETAPA 3 -----------------------
+@auth.requires_login()
+def etapa_3():
+    return dict(sTotal = session.sTotal, sTotal_F = double_real(session.sTotal).real())
+# ------------------ FIM DA ETAPA 3 ---------------------
+
+# @auth.requires_login()
+def clientes_retorno():
+    pattern = '%' + request.vars.itens + '%' #primeira letra maiusculo
+    selecionado = [row for row in db(db.clientes.nome.like(pattern)).select()] #select no db
+    #se a session nao existir criar ela
+
+    if not session.venda.codigo:
+        from datetime import datetime
+        now = datetime.now()
+        # session.venda.codigo =  "%s%s%s%s%s%s"% (now.second,now.minute,now.hour,now.day,now.month,now.year)
+        session.venda.codigo =  now.strftime("%y%m%d""%S%M%H")
+
+    
+        # response.flash = "Codigo da venda : %s"%session.vendaAtual_codigo  
+
+    for s in selecionado:
+        session.venda.nome = s.nome
+        session.venda.celular = s.celular
+        session.venda.email = s.email
+
+    # userDiv = UL(
+    #   LI(STRONG('Nome : '),SPAN(session.vendaAtual_nome,_id="nn"),_class="list-group-item"),
+    #   LI(STRONG('Celular : '),SPAN(session.vendaAtual_celuar),_class="list-group-item"),
+    #   LI(STRONG('E-mail : '),SPAN(session.vendaAtual_email),_class="list-group-item"),_class="list-group")
+
+# @auth.requires_login()
+def fecharVenda():
+    index = request.vars.transitory
+    index = index.split(";")
+    # dados a gravar no db
+    codigoVenda = session.codigo_venda
+    idCliente = db(Clientes.nome == session.cliente ).select('id')[0].id
+    
+    tipoVenda = index[0]
+    valorVenda = index[1]
+    valorDesconto = index[2]
+    #parcelados = parcelado(index[5])
+    # pegar o nome do representante e gravar o id no historico
+
+    representante = session.representante
+    enviarEmail = 'S' 
+    # Parcela, DataVencimento, Valor
+
+    # Parcela, DataVencimento, Valor
+    vendedor = session.auth.user.email #pegar usuario logado        
+    itensVenda = crud.select(Itens, Itens.codigoVenda == '%s'%codigoVenda,['codigoIten','quantidade','produto','valorUnidade','valorTotal'])
+    
+    if valorDesconto == '':
+        valorDesconto = '0.00'
+        pass
+    db.historicoVendas.insert(codigoVenda = codigoVenda,clienteEmail = idCliente,tipoVenda = tipoVenda,valorVenda = valorVenda,valorDesconto = valorDesconto,  vendedor = vendedor, representante = representante ) 
+    viewDesc = ""
+    if valorDesconto != "0.00":
+        valorT = (float(valorVenda) + float(valorDesconto))
+        viewDesc = "<h3><b>Total</b> : R$ %.2f - <b>Desconto</b> : <span>R$ %.2f</span></h3>"%(valorT, float(valorDesconto))
+
+    if enviarEmail == 'N':
+        enviar_email(codigoVenda)    
+
+    temp_codigoVenda = session.codigo_venda
+    session.__delitem__('codigo_venda')
+    session.__delitem__('cliente')
+    session.__delitem__('representante') 
+    pass
+#--------------------------------    
+
+
+
+
+def reenviar_email():
+    cod = request.vars.transitory
+    enviar_email(cod)
+
+
+def enviar_email(codigo):
+    historico = db(db.historicoVendas.codigoVenda == '%s'%codigo).select('tipoVenda','valorVenda','valorDesconto','dataVenda','clienteEmail')
+    desconto = historico[0].valorDesconto
+    total = historico[0].valorVenda
+    itens = crud.select(Itens, Itens.codigoVenda == '%s'%codigo,['codigoIten','quantidade','produto','valorUnidade','valorTotal'])
+    
+    subTotal = (float(total)+float(desconto))
+    tipoVenda = historico[0].tipoVenda 
+    # email = historico[0].clienteEmail
+    email = 'jose.vicente.de.sousa@gmail.com'
+    emailSimples = "|---------------- RECIBO DE COMPRA ----------------|\n" \
+    " ### ESSE DISPOSITIVO NAO E POSSIVEL VISUALIZAR OS DADOS ###\n" \
+    " ### Por gentileza visualize no seu email."
+
+    if desconto != 0.0:
+        mostrarDesconto = "[ Total =  R$ %.2f ] - [ Desconto = R$ %.2f ]"%(float(subTotal),float(desconto))
+    else:
+        mostrarDesconto = '' 
+    pass   
+
+    #------------ edit HTML email
+    emailHTML = '<div style="padding: 20px 29px;border: 1px solid #D2D2D2;border-radius: 11px;"><div class="adM"><br></div>'\
+    '<div style="text-align:center;border-bottom: 1px solid #DADADA;padding-bottom: 17px !important;">'\
+        '<img src="https://dl.dropboxusercontent.com/u/11469395/ArtesanalBaby/logo/logo-lojinha.png" width="95pt" class="CToWUd">'\
+    '</div>'\
+    '<p></p>'\
+    '<h2 style="color: #F78100;text-align: center;">Romaneio ArtesanalBaby ( codigo : %s )</h2>'\
+    '<p></p>'\
+    '<p>Recibo emitido em: %s</p>'\
+        '<div>'\
+            '%s'\
+            '%s'\
+        '</div>'\
+        '<h4><b>SUB-TOTAL</b> = R$ %.2f  |  <b>Tipo pag</b> : %s </h4>'\
+            '<p style="border-bottom: 1px solid #DADADA;padding-bottom: 17px !important;">Nos visite: <a href="http://www.artesanalbaby.com.br" target="_blank">www.artesanalbaby.com.br</a></p>'\
+            '<p></p>'\
+            '<h3 style="color: #B1B1B1;text-align: center;">Muito Obrigado pela compra! Volte sempre.</h3>'\
+            '<div class="yj6qo"></div>'\
+            '<div class="adL">'\
+                '<p></p>'\
+               ' <br>'\
+            '</div>'\
+        '</div></div>'%(codigo,(historico[0].dataVenda).strftime("%d/%m/%Y as %H:%M:%S"),itens,mostrarDesconto,float(total),tipoVenda)    
+    #------------ fim edit HTML email 
+
+    if mail:
+        if mail.send(to=["%s"%email],
+            subject='Romaneio ArtesanalBaby Cod:%s'%codigo,
+            message=[emailSimples, emailHTML]
+        ):
+            response.flash = 'Romaneio enviado a sucesso!'
+        else:
+            response.flash = 'Problema ao enviars o email!'
+    else:
+        response.flash = 'Unable to send the email : email parameters not defined' 
+     
+
